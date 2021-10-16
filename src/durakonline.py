c9a437f94461acee2f7ac9b3cda877750cfd3135
Zakovskiy <gogrugu@gmail.com>
2021-10-16 15:30:47 +0300

    ADD `objects.py` AND OTHER FUNCTIONS

--- src/durakonline.py
+++ src/durakonline.py
@@ -1,194 +1,511 @@
-import socket, json, threading, bitstring
+from utils import objects
+from datetime import datetime
+from loguru import logger
+import sys
+import socket
+import json
+import threading
+import bitstring
+import base64
+import hashlib
 
 class Client:
 
-	def __init__(self, token, p:int=11, device:str="xiaomi cactus", v:str="1.9.2", timezone:str="+03:00", _and:int=28, pl:str="android", language:str="ru", n:str="durak.android"):
-		self.p = p
-		self.device = device
-		self.version = v
-		self.timezone = timezone
-		self._and = _and
+	def __init__(self, token, pl:str="ios", debug:bool=False, language:str="ru"):
 		self.pl = pl
 		self.language = language
-		self.n = n
+		self.uid = None
 		self.receive = []
 		self.create_connection()
-		self.c()
-		self.sign()
-		self.signin_by_access_token(token)
+		logger.remove()
+		logger.add(sys.stderr, format="{time:HH:mm:ss.SSS}: {message}", level="DEBUG" if debug else "INFO")
+		if token:
+			self.sign(self.get_session_key().key)
+			self.signin_by_access_token(token)
 
 	def create_connection(self):
-		self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-		self.client_socket.settimeout(5000);
-		self.client_socket.connect(('65.21.92.165', 10783));
-		self.client_socket.settimeout(None);
+		self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.client_socket.settimeout(5000)
+		self.client_socket.connect(('65.21.92.165', 10783))
+		self.client_socket.settimeout(None)
 		threading.Thread(target=self.receive_messages).start()
 
 	def receive_messages(self):
+		buffer = ""
 		while True:
-			byteBuffe = self.client_socket.recv(1042)
-			byteBuffer = bitstring.BitStream()
-			byteBuffer.append(byteBuffe)
-			i = 0
-			i2 = 0
 			try:
-				str = byteBuffer.bytes.decode("utf8")
+				data = self.client_socket.recv(4096).decode()
 			except:
-				print("DEBUG<<EXCEPT")
-				pass
-			if (len(str) != 1 or not str.endswith("\n")):
-				split = str.split("\n")
-				if (len(split) != 1 or str.endswith("\n")):
-					byteBuffer._setbytepos(0)
-					while (True):
-						i = len(split) if (str.endswith("\n")) else len(split) - 1
-						if (i2 >= i):
-							break
-						if (len(split[i2]) > 0):
-							print("DEBUG>>"+split[i2])
-							self._split_on_command(split[i2])
-						i2+=1
-					if (len(split[len(split) - 1]) > 0 and not str.endswith("\n")):
-						byteBuffer.append(split[len(split) - 1].encode("UTF-8"))
-						continue
-					continue
 				continue
-			byteBuffer._setbytepos(0)
+			buffer += data
+			if not buffer.endswith('\n'):
+				continue
+			i1 = 0
+			for i in buffer.split('\n'):
+				i = i.replace('\n', '')
+				pos = i.find('{')
+				command = i[:pos]
+				try:
+					message = json.loads(i[pos:])
+				except Exception:
+					message = {}
+					continue
+				message['command'] = command
+				logger.debug(message)
+				self.receive.append(message)
+			buffer = ""
 
-	def _split_on_command(self, str:str):
-		index = str.find("{")
-		if (index != -1):
-			try:
-				bVar = json.loads(str[index:-1]+"}")
-			except:
-				bVar = {}
-			str = str[0:index]
+	def send_server(self, data:dir):
+		self.client_socket.send((data.pop('command')+json.dumps(data, separators=(',', ':')).replace("{}", '')+'\n').encode())
+
+	def get_session_key(self):
+		data = {
+			"command":"c",
+			"l":self.language,
+			"tz":"+02:00",
+			"t":datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]+"Z",
+			"pl":self.pl,
+			"p":10
+		}
+		if self.pl == "iphone":
+			data.update({
+				"v":"1.9.1.2",
+				"ios":"14.4",
+				"d":"iPhone8,4",
+				"n":"durak.ios"
+			})
 		else:
-			bVar = {}
-		if str == None:
-			return
-		self.receive.append([str, bVar])
+			data.update({
+				"v":"1.9.2",
+				"d":"xiaomi cactus",
+				"and":28,
+				"n":f"durak.{self.pl}"
+			})
+		self.send_server(
+			data
+		)
+		return objects.GetSessionKey(self.listen()).GetSessionKey
 
-	def c(self):
-		self.send_server("c", {"p":self.p, "d":self.device, "v":self.version, "tz":self.timezone, "and":self._and, "pl":self.pl, "l":self.language, "n":self.n})
-		return self.listen()
-
-	def sign(self, hash:str="4TpfGINb08B+x1vAll3ghQ=="):
-		self.send_server("sign", {"hash":hash})
+	def sign(self, key):
+		hash = base64.b64encode(hashlib.md5((key+"oc3q7ingf978mx457fgk4587fg847").encode()).digest()).decode()
+		self.send_server(
+			{
+				"command":"sign",
+				"hash":hash
+			}
+		)
 		return self.listen()
 
 	def signin_by_access_token(self, token):
 		self.token = token
-		self.send_server("auth", {"token":self.token})
-		return self.listen()
+		self.send_server(
+			{
+				"command":"auth",
+				"token":self.token
+			}
+		)
+		authorized = self.listen()
+		if authorized["command"] == "err":
+			raise objects.Err(authorized)
+		self.uid = authorized["id"]
+		return self.uid
 
-	def __send_server(self, msg):
-		msg += "\n";
-		self.client_socket.send(msg.encode());
+	def google_auth(self, id_token:str):
+		self.send_server(
+			{
+				"command":"durak_google_auth",
+				"id_token":id_token
+			}
+		)
 
-	def send_server(self, type, content:dir = None):
-		if not content:
-			self.__send_server(type)
-		else:
-			self.__send_server(type+json.dumps(content))
+	def get_captcha(self):
+		self.send_server(
+			{
+				"command":"get_captcha"
+			}
+		)
+		return self.__get_data("captcha")
+
+	def register(self, name, captcha:str=''):
+		self.send_server(
+			{
+				"command":"register",
+				"name":name,
+				"captcha":captcha
+			}
+		)
+		data = self.listen()
+		if data["command"] == "err": raise objects.Err(data)
+		else: return objects.Register(data).Register
 
 	def get_user_info(self, user_id:int):
-		self.send_server("get_user_info", {"id":user_id});
+		self.send_server(
+			{
+				"command":"get_user_info",
+				"id":user_id
+			}
+		)
+		data = self.__get_data("user_info")
+		if data["command"] == "err": raise objects.Err(data)
+		else: return objects.UserInfo(data).UserInfo
 
 	def friend_accept(self, friend_id:int):
-		self.send_server("friend_accept", {"id":friend_id});
+		self.send_server(
+			{
+				"command":"friend_accept",
+				"id":friend_id
+			}
+		)
 
 	def friend_delete(self, friend_id:int):
-		self.send_server("friend_delete", {"id":friend_id})
+		self.send_server(
+			{
+				"command":"friend_delete",
+				"id":friend_id
+			}
+		)
+
+	def verify_purchase(self, signature, purchase_data):
+		self.send_server(
+			{
+				"command":"verify_purchase",
+				"signature":signature,
+				"purchase_data":purchase_data
+			}
+		)
+
+	def get_purchase_ids(self):
+		self.send_server(
+			{
+				"command":"get_android_purchase_ids"
+			}
+		)
+		return objects.PurchaseIds(self.__get_data("android_purchase_ids")).PurchaseIds
+
+	def get_prem_price(self):
+		self.send_server(
+			{
+				"command":"get_prem_price"
+			}
+		)
+		return objects.ItemsPrice(self.__get_data("prem_price")).ItemsPrice
+
+	def get_points_price(self):
+		self.send_server(
+			{
+				"command":"get_points_price"
+			}
+		)
+		return objects.ItemsPrice(self.__get_data("points_price")).ItemsPrice
+
+	def buy_prem(self, id:int=0):
+		self.send_server(
+			{
+				"command":"buy_prem",
+				"id":f"com.rstgames.durak.prem.{id}"
+			}
+		)
 
 	def buy_points(self, id:int=0):
-		self.send_server("buy_points", {"id":f"com.rstgames.durak.points.{id}"})
+		self.send_server(
+			{
+				"command":"buy_points",
+				"id":f"com.rstgames.durak.points.{id}"
+			}
+		)
+
+		return self.listen()
 
 	def buy_asset(self, asset_id):
-		self.send_server("buy_asset", {"id":asset_id})
+		self.send_server(
+			{
+				"command":"buy_asset",
+				"id":asset_id
+			}
+		)
 
-	def friend_list(self):
-		self.send_server("friend_list")
+	def get_friend_list(self):
+		self.send_server(
+			{
+				"command":"friend_list"
+			}
+		)
+		friends = []
+		data = self.listen()
+		while data["command"] != "img_msg_price":
+			friends.append(objects.FriendInfo(data).FriendInfo)
+			data = self.listen()
 
-	def join(self, password:str, game_id):
-		self.send_server("join", {"password":password, "id":game_id})
+		return friends
 
-	def rejoin(self, position, game_id):
-		self.send_server("rejoin", {"p":position, "id":game_id})
+	def join_to_game(self, password:str, game_id):
+		self.send_server(
+			{
+				"command":"join",
+				"password":password,
+				"id":game_id
+			}
+		)
+
+	def rejoin_to_game(self, position, game_id):
+		self.send_server(
+			{
+				"command":"rejoin",
+				"p":position,
+				"id":game_id
+			}
+		)
 
 	def leave(self, game_id):
-		self.send_server("leave", {"id":game_id})
+		self.send_server(
+			{
+				"command":"leave",
+				"id":game_id
+			}
+		)
 
 	def game_publish(self):
-		self.send_server("game_publish")
+		self.send_server(
+			{
+				"command":"game_publish"
+			}
+		)
 
 	def get_assets(self):
-		self.send_server("get_assets")
+		self.send_server(
+			{
+				"command":"get_assets"
+			}
+		)
+		return objects.Assets(self.__get_data("assets")).Assets
 
 	def asset_select(self, asset_id):
-		self.send_server("asset_select", {"id":asset})
+		self.send_server(
+			{
+				"command":"asset_select",
+				"id":asset
+			}
+		)
 
 	def achieve_select(self, achieve_id):
-		self.send_server("achieve_select", {"id":achieve})
+		self.send_server(
+			{
+				"command":"achieve_select",
+				"id":achieve_id
+			}
+		)
 
 	def send_smile_game(self, smile_id:int=16):
-		self.send_server("smile", {"id":smile_id})
+		self.send_server(
+			{
+				"command":"smile",
+				"id":smile_id
+			}
+		)
 
 	def ready(self):
-		self.send_server("ready")
+		self.send_server(
+			{
+				"command":"ready"
+			}
+		)
 
 	def surrender(self):
-		self.send_server("surrender")
+		self.send_server(
+			{
+				"command":"surrender"
+			}
+		)
 
-	def player_swap(self, position):
-		self.send_server("player_swap", {"id":position})
+	def player_swap(self, position:int):
+		self.send_server(
+			{
+				"command":"player_swap",
+				"id":position
+			}
+		)
 
 	def send_message_friend(self, content, to):
-		self.send_server("send_user_msg", {"msg":content, "to":to})
+		self.send_server(
+			{
+				"command":"send_user_msg",
+				"msg":content,
+				"to":to
+			}
+		)
 
 	def send_user_message_code(self, code, content):
-		self.send_server("send_user_msg_code", {"code":code, "msg":content})
+		self.send_server(
+			{
+				"command":"send_user_msg_code",
+				"code":code,
+				"msg":content
+			}
+		)
 
 	def delete_messege(self, messege_id):
-		self.send_server("delete_msg", {"msg_id":messege_id})
+		self.send_server(
+			{
+				"command":"delete_msg",
+				"msg_id":messege_id
+			}
+		)
 
 	def gift_coll_item(self, item_id:id, coll_id:str, to:int):
-		self.send_server("gift_coll_item", {"item_id":item_id, "coll_id":coll_id,"to_id":to})
+		self.send_server(
+			{
+				"command":"gift_coll_item",
+				"item_id":item_id,
+				"coll_id":coll_id,
+				"to_id":to
+			}
+		)
 
-	def create_game(self, bet, password, players:int=3, deck:int=24, fast:bool=True):
-		self.send_server("create", {"bet":bet,"password":password,"fast":fast,"sw":True,"nb":True,"ch":False,"players":players,"deck":deck,"dr":True});
+	def get_bets(self):
+		self.send_server(
+			{
+				"command":"gb"
+			}
+		)
+		return objects.Bets(self.__get_data("bets")).Bets
+
+	def create_game(self, bet, password:str="", players:int=3, deck:int=24, fast:bool=True):
+		self.send_server(
+			{
+				"command":"create",
+				"bet":bet,
+				"password":password,
+				"fast":fast,
+				"sw":True,
+				"nb":True,
+				"ch":False,
+				"players":players,
+				"deck":deck,
+				"dr":True
+			}
+		)
+
+		data = self.__get_data("create")
+		if data["command"] == 'err': raise objects.Err(data)
+		else: return objects.Game(data).Game 
 
 	def invite_to_game(self, user_id):
-		self.send_server("invite_to_game", {"user_id":user_id})
+		self.send_server(
+			{
+				"command":"invite_to_game",
+				"user_id":user_id
+			}
+		)
 
 	def lookup_start(self, betMin:int=100, pr:bool=False, betMax:int=2500, fast:bool=True, sw:bool=True, nb:list=[False,True], ch:bool=False, players:list=[2,3,4,5,6], deck:list=[24,36,52], dr:bool=True):
-		self.send_server("lookup_start", {"betMin":betMin, "pr":[pr, False], "betMax":betMax, "fast":[fast], "sw":[sw], "nb":nb, "ch":[ch], "players":players, "deck":deck, "dr":[dr], "status":"open"})
+		self.send_server(
+			{
+				"command":"lookup_start",
+				"betMin":betMin,
+				"pr":[pr, False],
+				"betMax":betMax,
+				"fast":[fast],
+				"sw":[sw],
+				"nb":nb,
+				"ch":[ch],
+				"players":players,
+				"deck":deck,
+				"dr":[dr],
+				"status":"open"
+			}
+		)
 
 	def lookup_stop(self):
-		self.send_server("lookup_stop")
+		self.send_server(
+			{
+				"command":"lookup_stop"
+			}
+		)
 
 	def get_server(self):
-		self.send_server("get_server")
+		self.send_server(
+			{
+				"command":"get_server"
+			}
+		)
 
 	def update_name(self, nickname:str=None):
-		self.send_server("update_name", {"value":nickname})
+		self.send_server(
+			{
+				"command":"update_name",
+				"value":nickname
+			}
+		)
 
 	def save_note(self, note:str, user_id:int, color:int=0):
-		self.send_server("save_note", {"note":note, "color":color, "id":user_id})
+		self.send_server(
+			{
+				"command":"save_note",
+				"note":note,
+				"color":color,
+				"id":user_id
+			}
+		)
 
 	def leaderboard_get_by_user(self, user_id, type:str="score", season:bool=False):
 		s = "" if not season else "s_"
-		self.send_server(s+"lb_get_by_user", {"user_id":user_id, "type":type})
+		self.send_server(
+			{
+				"command":s+"lb_get_by_user",
+				"user_id":user_id,
+				"type":type
+			}
+		)
 
 	def leaderboard_get_top(self, type:str="score"):
-		self.send_server("lb_get_top", {"type":type})
+		self.send_server(
+			{
+				"command":"lb_get_top",
+				"type":type
+			}
+		)
 
 	def leaderboard_get_by_place_down(self, place:int=20, type:str="score"):
-		self.send_server("lb_get_by_place_down", {"place":place, "type":type})
+		self.send_server(
+			{
+				"command":"lb_get_by_place_down",
+				"place":place,
+				"type":type
+			}
+		)
+
+	def turn(self, card):
+		self.send_server(
+			{
+				"command":"t",
+				"c":card
+			}
+		)
+
+	def take(self):
+		self.send_server(
+			{
+				"command":"take"
+			}
+		)
+
+	def _pass(self):
+		self.send_server(
+			{
+				"command":"pass"
+			}
+		)
 
 	def listen(self):
-		while (len(self.receive) == 0):
-			pass;
+		while len(self.receive) == 0:
+			pass
 		r = self.receive[0]
 		del self.receive[0]
-		return [r[0], r[1]]
+		return r
+
+	def __get_data(self, type):
+		data = self.listen()
+		while 1:
+			if data["command"] in [type,"err"]:
+				return data
+			data = self.listen()
---
+++ src/utils/objects.py
@@ -0,0 +1,299 @@
+class Err(Exception):
+
+	def __init__ (self, data:dir):
+		self.json = data
+		try: self.code = data["code"]
+		except: self.code = None
+
+class GetSessionKey:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.key = None
+
+	@property
+	def GetSessionKey (self):
+		self.key = self.json["key"]
+		return self
+
+class Server:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.time = None
+		self.id = None
+
+	@property
+	def Server (self):
+		self.time = self.json["time"]
+		self.id = self.json["id"]
+		return self
+
+class SigninByAccessToken:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.id = None
+
+	@property
+	def SigninByAccessToken(self):
+		self.id = self.json["id"]
+		return self
+
+class Register:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.token = None
+
+	@property
+	def Register(self):
+		self.token = self.json["token"]
+		return self
+
+class User:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.id = None
+		self.name = None
+		self.avatar = None
+		self.dtp = None
+		self.frame = None
+		self.score = None
+		self.achieve = None
+		self.pw = None
+
+	@property
+	def User(self):
+		self.id = self.json["id"]
+		self.name = self.json["name"]
+		self.avatar = self.json["avatar"]
+		self.dtp = self.json["dtp"]
+		self.frame = self.json["frame"]
+		self.score = self.json["score"]
+		self.achieve = self.json["achieve"]
+		self.pw = self.json["pw"]
+		return self
+	
+
+class FriendInfo:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.user : User = User(data["user"]).User
+		self.kind = None
+		self.new = None
+
+	@property
+	def FriendInfo(self):
+		self.kind = self.json["kind"]
+		self.new = self.json["new"]
+		return self
+	
+
+class UserInfo:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.id = None
+		self.name = None
+		self.avatar = None
+		self.pw = None
+		self.ach = []
+		self.ach_c = None
+		self.t_bronze = None
+		self.t_silver = None
+		self.t_gold = None
+		self.wins = None
+		self.wins_season = None
+		self.points_win = None
+		self.points_win_season = None
+		self.score = None
+		self.score_season = None
+		self.dtp = None
+		self.frame = None
+		self.assets = []
+		self.achieve = None
+		self.achieves = []
+		self.coll = {}
+
+	@property
+	def UserInfo(self):
+		self.id = self.json["id"]
+		self.name = self.json["name"]
+		self.avatar = self.json["avatar"]
+		self.pw = self.json["pw"]
+		self.ach = self.json["ach"]
+		self.ach_c = self.json["achc"]
+		self.t_bronze = self.json["t_bronze"]
+		self.t_silver = self.json["t_silver"]
+		self.t_gold = self.json["t_gold"]
+		self.wins = self.json["wins"]
+		self.wins_season = self.json["wins_s"]
+		self.points_win = self.json["points_win"]
+		self.points_win_season = self.json["points_win_s"]
+		self.score = self.json["score"]
+		self.score_season = self.json["score_s"]
+		self.dtp = self.json["dtp"]
+		self.frame = self.json["frame"]
+		self.assets = self.json["assets"]
+		self.achieve = self.json["achieve"]
+		self.achieves = self.json["achieves"]
+		self.coll = self.json["coll"]
+		return self
+
+class Smile:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.id = None
+		self.index = None
+		self.mask = None
+		self.level = None
+		self.price = None
+		self.name = None
+		self.description = None
+
+	@property
+	def Smile(self):
+		self.id = self.json["id"]
+		self.index = self.json["index"]
+		self.mask = self.json["mask"]
+		self.level = self.json["level"]
+		self.price = self.json["price"]
+		self.name = self.json["name"]["ru"]
+		self.description = self.json["desc"]["ru"]
+		return self
+
+class Frame:
+
+	def __init__ (self, data:dir):
+		self.json = data
+
+	@property
+	def Frame(self):
+		return Shirt(self.json).Shirt
+
+
+class Shirt:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.id = None
+		self.index = None
+		self.mask = None
+		self.level = None
+		self.price = None
+		self.name = None
+		self.description = None
+		self.hidden = None
+		self.group = None
+
+	@property
+	def Shirt(self):
+		self.id = self.json["id"]
+		self.index = self.json["index"]
+		self.mask = self.json["mask"]
+		self.level = self.json["level"]
+		self.price = self.json["price"]
+		self.name = self.json["name"]["ru"]
+		try: self.description = self.json["desc"]["ru"]
+		except: pass
+		self.hidden = self.json["hidden"]
+		self.group = self.json["group"]
+		return self
+
+class Assets:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.smile = []
+		self.shirt = []
+
+	@property
+	def Assets (self):
+		for smile in self.json["smile"]:
+			self.smile.append(Smile(smile).Smile)
+		return self
+
+class ItemsPrice:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.ids = []
+
+	@property
+	def ItemsPrice(self):
+		for id in self.json["ids"]:
+			self.ids.append(ItemPrice(id).ItemPrice)
+		return self
+
+class PurchaseIds:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.ids = None
+
+	@property
+	def PurchaseIds(self):
+		self.ids = self.json["ids"]
+		return self
+
+class Bets:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.v = []
+
+	@property
+	def Bets(self):
+		self.v = self.json["v"]
+		return self
+	
+
+class ItemPrice:
+
+	def __init__(self, data:dir):
+		self.json = data
+		self.price = None
+		self.quantity = None
+		self.id = None
+
+	@property
+	def ItemPrice(self):
+		self.price = self.json["price"]
+		self.quantity = self.json["quantity"]
+		self.id = self.json["id"]
+		return self
+
+class Game:
+
+	def __init__ (self, data:dir):
+		self.json = data
+		self.id = None
+		self.players = None
+		self.position = None
+		self.deck = None
+		self.timeout = None
+		self.sw = None
+		self.ch = None
+		self.dr = None
+		self.nb = None
+		self.bet = None
+		self.fast = None
+
+	@property
+	def Game(self):
+		self.id = self.json["id"]
+		self.players = self.json["players"]
+		self.position = self.json["position"]
+		self.deck = self.json["deck"]
+		self.timeout = self.json["timeout"]
+		self.sw = self.json["sw"]
+		self.ch = self.json["ch"]
+		self.dr = self.json["dr"]
+		self.nb = self.json["nb"]
+		self.bet = self.json["bet"]
+		self.fast = self.json["fast"]
+		return self
