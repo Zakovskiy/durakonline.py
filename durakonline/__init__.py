22ab9c97de9e79f46a88ac5fe0ac02418f9565b8
zakovskiy <60068872+Zakovskiy@users.noreply.github.com>
2022-03-14 18:26:17 +0300

    3.2.1

---
+++ durakonline/__init__.py
@@ -0,0 +1,20 @@
+__title__ = "durakonline.py"
+__author__ = "Zakovskiy"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020-2022 Slimakoi"
+__version__ = "3.2.1"
+
+from .authorization import Authorization
+from .durakonline import Client
+from .game import Game
+from .socket_listener import SocketListener
+
+from .utils import objects
+
+from requests import get
+from json import loads
+
+__newest__ = loads(get("https://pypi.python.org/pypi/durakonline.py/json").text)["info"]["version"]
+
+if __version__ != __newest__:
+    exit(f"New version of {__title__} available: {__newest__} (Using {__version__})")
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+setup(
+    name = "durakonline.py",
+    version = "3.2.1",
+    url = "https://github.com/Zakovskiy/durakonline.py",
+    download_url = "https://github.com/Zakovskiy/durakonline.py/tarball/master",
+    license = "MIT",
+    author = "Zakovskiy",
+    author_email = "gogrugu@gmail.com",
+    description = "A library to create Durak Online bots.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    keywords = [
+        "durak",
+        "online",
+        "durakonline",
+        "durakonline.py",
+        "durakonline-bot",
+        "rstgame",
+        "rstgames",
+        "api",
+        "socket",
+        "python",
+        "python3",
+        "python3.x",
+        "zakovskiy",
+        "official"
+    ],
+    install_requires = [
+        "setuptools",
+        "requests",
+        "loguru",
+        "websockets",
+        "websocket-client==0.57.0",
+        "aiohttp"
+    ],
+    setup_requires = [
+        "wheel"
+    ],
+    packages = find_packages()
+)
