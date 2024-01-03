59661099808e9bab51889f7870a0de3a930351dd
root <gogrugu@gmail.com>
2024-01-03 14:55:06 +0300

    Modify bot.py

--- examples/bot.py
+++ examples/bot.py
@@ -1,13 +1,12 @@
-import durakonline, json
+import json
+from durakonline import durakonline
 
-Durak = durakonline.Client("access_token")
+Durak = durakonline.Client()
+Durak.authorization.signin_by_access_token("access_token")
 
 @Durak.event(command="user_msg")
 def event(data):
-    try:
-        user_id = data["from"]
-    except:
-        user_id = Durak.uid
+    user_id = data.get("from", Durak.uid)
     to = data["from"] if data["to"] == Durak.uid else data["to"]
     user_name = data["name"]
     Durak.send_message_friend(f">>{user_name}, hello!", to)
