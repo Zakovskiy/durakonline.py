c0af26d07ce95fe76f1236f3e26bb69128c39e29
zak <60068872+Zakovskiy@users.noreply.github.com>
2024-01-15 23:47:13 +0300

    Update bot.py
    fix

--- examples/bot.py
+++ examples/bot.py
@@ -5,8 +5,8 @@ Durak = durakonline.Client()
 Durak.authorization.signin_by_access_token("access_token")
 
 @Durak.event(command="user_msg")
-def event(data):
+def event(data: dict):
     user_id = data.get("from", Durak.uid)
     to = data["from"] if data["to"] == Durak.uid else data["to"]
     user_name = data["name"]
-    Durak.send_message_friend(f">>{user_name}, hello!", to)
+    Durak.friend.send_message(f">>{user_name}, hello!", to)
